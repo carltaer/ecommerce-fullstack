d3a69fce94351f7ea07c34797c9d0ce4a50b1308
carltaer <carlchristopher.taer@gmail.com>
2023-02-17 19:52:05 +0000

    edit orders and users to a valid json file

--- jct/backend/data/orders.js
+++ jct/backend/data/orders.js
@@ -8,35 +8,35 @@ const orders = [
                 "qty": 2,
                 "image": "https://example.com/product1.jpg",
                 "price": 20.99,
-                "item": 1,
+                "item": 1
             },
             {
                 "name": "Product 2",
                 "qty": 1,
                 "image": "https://example.com/product2.jpg",
                 "price": 9.99,
-                "item": 2,
+                "item": 2
             }
         ],
         "shippingAddress": {
             "address": "123 Main St",
             "city": "Anytown",
             "postalCode": "12345",
-            "country": "USA",
+            "country": "USA"
         },
         "paymentMethod": "Credit Card",
         "paymentResult": {
             "id": "PAYMENT_ID",
             "status": "COMPLETED",
             "update_time": "2023-02-15T12:34:56Z",
-            "email_address": "john@example.com",
+            "email_address": "john@example.com"
         },
         "taxPrice": 2.50,
         "shippingPrice": 5.00,
         "totalPrice": 37.47,
         "isPaid": true,
         "paidAt": "2023-02-15",
-        "deliveredAt": "2023-02-22",
+        "deliveredAt": "2023-02-22"
     },
 
     {
@@ -48,28 +48,28 @@ const orders = [
                 "qty": 1,
                 "image": "https://example.com/product3.jpg",
                 "price": 15.99,
-                "item": 3,
+                "item": 3
             }
         ],
         "shippingAddress": {
             "address": "456 Elm St",
             "city": "Anycity",
             "postalCode": "67890",
-            "country": "USA",
+            "country": "USA"
         },
         "paymentMethod": "PayPal",
         "paymentResult": {
             "id": "PAYMENT_ID",
             "status": "COMPLETED",
             "update_time": "2023-02-14T23:45:01Z",
-            "email_address": "jane@example.com",
+            "email_address": "jane@example.com"
         },
         "taxPrice": 1.60,
         "shippingPrice": 4.50,
         "totalPrice": 22.09,
         "isPaid": true,
         "paidAt": "2023-02-14",
-        "deliveredAt": "2023-02-20",
+        "deliveredAt": "2023-02-20"
     },
     
     {
@@ -81,42 +81,42 @@ const orders = [
                 "qty": 3,
                 "image": "https://example.com/product4.jpg",
                 "price": 7.99,
-                "item": 4,
+                "item": 4
             },
             {
                 "name": "Product 5",
                 "qty": 1,
                 "image": "https://example.com/product5.jpg",
                 "price": 14.99,
-                "item": 5,
+                "item": 5
             },
             {
                 "name": "Product 6",
                 "qty": 2,
                 "image": "https://example.com/product6.jpg",
                 "price": 12.99,
-                "item": 6,
+                "item": 6
             }
         ],
         "shippingAddress": {
             "address": "456 Elm St",
             "city": "Anycity",
             "postalCode": "67890",
-            "country": "USA",
+            "country": "USA"
         },
         "paymentMethod": "PayPal",
         "paymentResult": {
             "id": "PAYMENT_ID",
             "status": "COMPLETED",
             "update_time": "2023-02-14T23:45:01Z",
-            "email_address": "jane@example.com",
+            "email_address": "jane@example.com"
         },
         "taxPrice": 1.60,
         "shippingPrice": 4.50,
         "totalPrice": 22.09,
         "isPaid": true,
         "paidAt": "2023-02-14",
-        "deliveredAt": "2023-02-20",
+        "deliveredAt": "2023-02-20"
     },
 
     {
@@ -128,43 +128,43 @@ const orders = [
                 "qty": 3,
                 "image": "https://example.com/product4.jpg",
                 "price": 7.99,
-                "item": 4,
+                "item": 4
             },
             {
                 "name": "Product 5",
                 "qty": 1,
                 "image": "https://example.com/product5.jpg",
                 "price": 14.99,
-                "item": 5,
+                "item": 5
             },
             {
                 "name": "Product 6",
                 "qty": 2,
                 "image": "https://example.com/product6.jpg",
                 "price": 12.99,
-                "item": 6,
+                "item": 6
             }
         ],
         "shippingAddress": {
             "address": "456 Elm St",
             "city": "Anycity",
             "postalCode": "67890",
-            "country": "USA",
+            "country": "USA"
         },
         "paymentMethod": "PayPal",
         "paymentResult": {
             "id": "PAYMENT_ID",
             "status": "COMPLETED",
             "update_time": "2023-02-14T23:45:01Z",
-            "email_address": "jane@example.com",
+            "email_address": "jane@example.com"
         },
         "taxPrice": 1.60,
         "shippingPrice": 4.50,
         "totalPrice": 22.09,
         "isPaid": true,
         "paidAt": "2023-02-14",
-        "deliveredAt": "2023-02-20",
-    },
+        "deliveredAt": "2023-02-20"
+    }
 ]
 
 export default orders
--- jct/backend/data/users.js
+++ jct/backend/data/users.js
@@ -1,39 +1,39 @@
 const users  = [
-    {
-        "id": "1",
-        "name": "Alice",
-        "email": "alice@example.com",
-        "password": "password123",
-        "isAdmin": false,
-    },
-    {
-        "id": "2",
-        "name": "Bob",
-        "email": "bob@example.com",
-        "password": "qwertyuiop",
-        "isAdmin": true,
-    },
-    {
-        "id": "3",
-        "name": "Charlie",
-        "email": "charlie@example.com",
-        "password": "password456",
-        "isAdmin": false,
-    },
-    {
-        "id": "4",
-        "name": "Dave",
-        "email": "dave@example.com",
-        "password": "123456789",
-        "isAdmin": true,
-    },
-    {
-        "id": "v5",
-        "name": "Eve",
-        "email": "eve@example.com",
-        "password": "password789",
-        "isAdmin": false,
-    },
+	{
+		"id": "1",
+		"name": "Alice",
+		"email": "alice@example.com",
+		"password": "password123",
+		"isAdmin": false
+	},
+	{
+		"id": "2",
+		"name": "Bob",
+		"email": "bob@example.com",
+		"password": "qwertyuiop",
+		"isAdmin": true
+	},
+	{
+		"id": "3",
+		"name": "Charlie",
+		"email": "charlie@example.com",
+		"password": "password456",
+		"isAdmin": false
+	},
+	{
+		"id": "4",
+		"name": "Dave",
+		"email": "dave@example.com",
+		"password": "123456789",
+		"isAdmin": true
+	},
+	{
+		"id": "v5",
+		"name": "Eve",
+		"email": "eve@example.com",
+		"password": "password789",
+		"isAdmin": false
+	}
 ]
 
 export default users
